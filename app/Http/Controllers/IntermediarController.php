7d79d38aed6c7b7850351b26f6da335880b674b2
luisrovirac <luisrovirac@gmail.com>
2024-01-18 11:54:49 -0400

    add app basic laravel api

---
+++ app/Http/Controllers/IntermediarController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\intermediar;
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+
+use Illuminate\Support\Facades\Http;
+
+
+class IntermediarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(intermediar $intermediar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, intermediar $intermediar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(intermediar $intermediar)
+    {
+        //
+    }
+
+	public function consumeapidemo(){
+		$client = new Client([
+			'base_uri' => 'https://jsonplaceholder.typicode.com',
+		]);
+		$response = $client->request('GET', 'posts');
+		return json_decode($response->getBody()->getContents());
+	}
+}
